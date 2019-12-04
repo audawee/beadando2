0dd8cd7ada0a346f59f0147307f34a80e3f5c953
Audawee <modokur18@gmail.com>
2019-12-04 01:30:08 +0100

    reg/login

---
+++ head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+</head>
+<body>
+
+<div class="navbar">
+			<div class="logo_div">
+				<a href="index.php" style="text-decoration: none"><h1>BEADANDO</h1></a>
+			</div>
+			<ul>
+			  <li><a href="index.php">Home</a></li>
+			  <li><a href="comments.php">Comments</a></li>
+			  <li><a href="register.php">Registartion</a></li>
+			  <li><a href="login.php">Login</a></li>
+			</ul>
+		</div>
